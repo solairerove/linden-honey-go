6977608589676ea2d3fd1ceef054aabcf36e6e32
Mikita Kryvitski <solairerove@gmail.com>
2019-08-04 14:56:25 +0300

    feature: .env support added

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 const (
-	dbUsername = "linden-honey-user"
-	dbPassword = "linden-honey-pass"
-	dbName     = "linden-honey"
-	dbPort     = "5430"
+	usingLindenHoneyScraper = "USING_LINDEN_HONEY_SCRAPER"
+	dbUsername              = "linden-honey-user"
+	dbPassword              = "linden-honey-pass"
+	dbName                  = "linden-honey"
+	dbPort                  = "5430"
 )
 
 func main() {
 	log.Println("Nothing else matters")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	log.Println(usingLindenHoneyScraper, os.Getenv(usingLindenHoneyScraper))
 }
