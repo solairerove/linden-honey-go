0b4a173e3a8d61f09db9cc43cbcd2ed472a55abe
Mikita Kryvitski <solairerove@gmail.com>
2019-08-11 23:53:07 +0300

    feature: just save one song

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/joho/godotenv"
 	"github.com/solairerove/linden-honey-go/model"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
 	"os"
@@ -23,11 +26,49 @@ const (
 func main() {
 	log.Println("Nothing else matters")
 
+	// load env
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file", err)
 	}
 
+	// TODO: rewrite and move to package after go web udemy
+	// new mongodb client
+	var auth options.Credential
+	auth.Username = dbUsername
+	auth.Password = dbPassword
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017").SetAuth(auth))
+
+	if err != nil {
+		log.Fatal("Error connecting to mongodb: ", err)
+	}
+
+	// connect to context?
+	err = client.Connect(context.Background())
+	if err != nil {
+		log.Fatal("What the fuck is context todo: ", err)
+	}
+
+	// check connection
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal("What the fuck is context todo ping: ", err)
+	}
+
+	// TODO: to .env
+	collection := client.Database("linden_honey").Collection("songs")
+
+	// close connection
+	defer func() {
+		err := client.Disconnect(context.Background())
+		if err != nil {
+			log.Fatal("Error closing mongo client: ", err)
+		}
+	}()
+	log.Println("Connection to MongoDB closed.")
+
+	// get scraper bool from .env file
 	b, err := strconv.ParseBool(os.Getenv(usingLindenHoneyScraper))
 	if err != nil {
 		log.Fatal("Can't parse to bool value: ", err)
@@ -38,24 +79,35 @@ func main() {
 	scraperURL := fmt.Sprintf("%s/songs", os.Getenv(lindenHoneyScraperURL))
 	log.Println(lindenHoneyScraperURL, scraperURL)
 
-	resp, err := http.Get(scraperURL)
-	if err != nil {
-		log.Fatal("Error loading data from scraper: ", err)
-	}
-
-	defer func() {
-		err := resp.Body.Close()
+	// TODO: rewrite and move to package after go web udemy
+	// fetch and update collection if necessary
+	if b {
+		resp, err := http.Get(scraperURL)
 		if err != nil {
-			log.Fatal("Error closing body from scraper: ", err)
+			log.Fatal("Error loading data from scraper: ", err)
 		}
-	}()
 
-	songs := make([]model.Song, 0)
+		defer func() {
+			err := resp.Body.Close()
+			if err != nil {
+				log.Fatal("Error closing body from scraper: ", err)
+			}
+		}()
 
-	err = json.NewDecoder(resp.Body).Decode(&songs)
-	if err != nil {
-		log.Fatal("Error unmarshalling data from scraper: ", err)
+		songs := make([]model.Song, 0)
+
+		err = json.NewDecoder(resp.Body).Decode(&songs)
+		if err != nil {
+			log.Fatal("Error unmarshalling data from scraper: ", err)
+		}
+
+		log.Println(len(songs))
+
+		insertResult, err := collection.InsertOne(context.Background(), songs[0])
+		if err != nil {
+			log.Fatal("Error persisting songs into mongodb: ", err)
+		}
+
+		fmt.Println("Inserted multiple documents: ", insertResult.InsertedID)
 	}
-
-	log.Println(len(songs))
 }
--- model/song.go
+++ model/song.go
@@ -1,12 +1,7 @@
 package model
 
-import (
-	uuid "github.com/satori/go.uuid"
-)
-
 // Song ... tbd
 type Song struct {
-	ID     uuid.NullUUID `sql:",pk,type:uuid default uuid_generate_v4()" json:"-"`
 	Title  string        `json:"title,omitempty"`
 	Link   string        `json:"link,omitempty"`
 	Author string        `json:"author,omitempty"`
@@ -16,8 +11,6 @@ type Song struct {
 
 // Verse ... tbd
 type Verse struct {
-	ID     uuid.NullUUID `sql:",pk,type:uuid default uuid_generate_v4()" json:"-"`
 	Order  int           `json:"order"`
 	Verse  string        `json:"verse,omitempty"`
-	SongID uuid.NullUUID `sql:",type:uuid" json:"-"`
 }
